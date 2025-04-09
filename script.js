e2568cf8db28de77a7fa33d078034121798d57f1
simoneputeo <s.puteo@gmail.com>
2025-04-09 16:05:33 +0200

    done: to fix: on and off bulbs misaligned

---
+++ script.js
@@ -0,0 +1,25 @@
+const lightSwitch = document.getElementById("accendi");
+const lightBulb0 = document.getElementById("lightoff");
+const lightBulb1 = document.getElementById("lighton");
+
+lightSwitch.addEventListener("click", function(){
+    if (lightSwitch.classList.contains("btn-warning"))
+        {
+        lightSwitch.classList.replace("btn-warning", "btn-dark");
+        lightBulb0.classList.replace("d-block","d-none");
+        lightBulb1.classList.replace("d-none","d-block");
+        lightSwitch.textContent = "Spegni la luce"
+
+    }
+    else 
+    {
+        lightSwitch.classList.replace("btn-dark","btn-warning")
+        lightBulb0.classList.replace("d-none","d-block")
+        lightBulb1.classList.replace("d-block","d-none");
+        lightSwitch.textContent = "Accendila luce"
+
+
+    }
+    }
+
+)
